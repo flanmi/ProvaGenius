d5ecc1bbe8c655c6381ff2c15b452f356dfe63ab
felipe bruno madeira <flanmi@gmail.com>
2017-03-13 14:22:53 -0300

    primeiro commit

---
+++ Source/ProvaGenius/ProvaGeniusGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "ProvaGeniusGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROVAGENIUS_API AProvaGeniusGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
